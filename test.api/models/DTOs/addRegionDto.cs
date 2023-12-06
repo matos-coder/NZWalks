9dea31a7b65851d52da17912bf475c97dc5dcf69
matos <matiasashenafi0@gmail.com>
2023-12-06 16:51:54 +0300

    validation

--- test.api/models/DTOs/addRegionDto.cs
+++ test.api/models/DTOs/addRegionDto.cs
@@ -1,9 +1,12 @@
-﻿namespace test.api.models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test.api.models.DTOs
 {
     public class addRegionDto
     {
+        [Required]
         public string Name { get; set; }
-
+        [Required]
         public string Code { get; set; }
 
         public string? RegionImgUrl { get; set; }
